a21758cbc28fbc91f63ff0bf627cc80d4386d543
mikimiki4748 <hibiki_taniguchi_4748@outlook.jp>
2017-12-13 23:59:39 +0900

    add MainPage

--- Gathers/Gathers/MainFrame.Designer.cs
+++ Gathers/Gathers/MainFrame.Designer.cs
@@ -29,12 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrame));
-            this.MainTab = new System.Windows.Forms.TabControl();
-            this.SoarceCode = new System.Windows.Forms.TabPage();
-            this.Question = new System.Windows.Forms.TabPage();
-            this.Q_and_A = new System.Windows.Forms.TabPage();
-            this.reference = new System.Windows.Forms.TabPage();
-            this.Gathers_Editor = new System.Windows.Forms.TabPage();
             this.toolbar = new System.Windows.Forms.ToolStrip();
             this.新規作成NToolStripButton = new System.Windows.Forms.ToolStripButton();
             this.開くOToolStripButton = new System.Windows.Forms.ToolStripButton();
@@ -46,72 +40,21 @@
             this.貼り付けPToolStripButton = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.ヘルプLToolStripButton = new System.Windows.Forms.ToolStripButton();
-            this.MainTab.SuspendLayout();
+            this.reference = new System.Windows.Forms.TabPage();
+            this.Q_and_A = new System.Windows.Forms.TabPage();
+            this.Question = new System.Windows.Forms.TabPage();
+            this.SoarceCode = new System.Windows.Forms.TabPage();
+            this.MainPage = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.MainTab = new System.Windows.Forms.TabControl();
+            this.label2 = new System.Windows.Forms.Label();
             this.toolbar.SuspendLayout();
+            this.MainPage.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.MainTab.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // MainTab
-            // 
-            this.MainTab.Controls.Add(this.SoarceCode);
-            this.MainTab.Controls.Add(this.Question);
-            this.MainTab.Controls.Add(this.Q_and_A);
-            this.MainTab.Controls.Add(this.reference);
-            this.MainTab.Controls.Add(this.Gathers_Editor);
-            this.MainTab.Location = new System.Drawing.Point(1, 25);
-            this.MainTab.Name = "MainTab";
-            this.MainTab.SelectedIndex = 0;
-            this.MainTab.Size = new System.Drawing.Size(1920, 1080);
-            this.MainTab.TabIndex = 0;
-            // 
-            // SoarceCode
-            // 
-            this.SoarceCode.Location = new System.Drawing.Point(4, 22);
-            this.SoarceCode.Name = "SoarceCode";
-            this.SoarceCode.Padding = new System.Windows.Forms.Padding(3);
-            this.SoarceCode.Size = new System.Drawing.Size(1912, 1054);
-            this.SoarceCode.TabIndex = 0;
-            this.SoarceCode.Text = "ソースコード共有";
-            this.SoarceCode.UseVisualStyleBackColor = true;
-            // 
-            // Question
-            // 
-            this.Question.Location = new System.Drawing.Point(4, 22);
-            this.Question.Name = "Question";
-            this.Question.Padding = new System.Windows.Forms.Padding(3);
-            this.Question.Size = new System.Drawing.Size(1912, 1054);
-            this.Question.TabIndex = 1;
-            this.Question.Text = "Gathers道場";
-            this.Question.UseVisualStyleBackColor = true;
-            // 
-            // Q_and_A
-            // 
-            this.Q_and_A.Location = new System.Drawing.Point(4, 22);
-            this.Q_and_A.Name = "Q_and_A";
-            this.Q_and_A.Padding = new System.Windows.Forms.Padding(3);
-            this.Q_and_A.Size = new System.Drawing.Size(1912, 1054);
-            this.Q_and_A.TabIndex = 2;
-            this.Q_and_A.Text = "質問スレ";
-            this.Q_and_A.UseVisualStyleBackColor = true;
-            // 
-            // reference
-            // 
-            this.reference.Location = new System.Drawing.Point(4, 22);
-            this.reference.Name = "reference";
-            this.reference.Padding = new System.Windows.Forms.Padding(3);
-            this.reference.Size = new System.Drawing.Size(1912, 1054);
-            this.reference.TabIndex = 3;
-            this.reference.Text = "リファレンス共有";
-            this.reference.UseVisualStyleBackColor = true;
-            // 
-            // Gathers_Editor
-            // 
-            this.Gathers_Editor.Location = new System.Drawing.Point(4, 22);
-            this.Gathers_Editor.Name = "Gathers_Editor";
-            this.Gathers_Editor.Padding = new System.Windows.Forms.Padding(3);
-            this.Gathers_Editor.Size = new System.Drawing.Size(1912, 1054);
-            this.Gathers_Editor.TabIndex = 4;
-            this.Gathers_Editor.Text = "独自エディタ";
-            this.Gathers_Editor.UseVisualStyleBackColor = true;
             // 
             // toolbar
             // 
@@ -130,7 +73,7 @@
             this.toolbar.Location = new System.Drawing.Point(0, 0);
             this.toolbar.Name = "toolbar";
             this.toolbar.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
-            this.toolbar.Size = new System.Drawing.Size(1486, 25);
+            this.toolbar.Size = new System.Drawing.Size(867, 25);
             this.toolbar.TabIndex = 1;
             this.toolbar.Text = "ツールバー";
             // 
@@ -216,11 +159,133 @@
             this.ヘルプLToolStripButton.Size = new System.Drawing.Size(23, 22);
             this.ヘルプLToolStripButton.Text = "ヘルプ(&L)";
             // 
+            // reference
+            // 
+            this.reference.Location = new System.Drawing.Point(4, 22);
+            this.reference.Name = "reference";
+            this.reference.Padding = new System.Windows.Forms.Padding(3);
+            this.reference.Size = new System.Drawing.Size(858, 474);
+            this.reference.TabIndex = 3;
+            this.reference.Text = "リファレンス共有";
+            this.reference.UseVisualStyleBackColor = true;
+            // 
+            // Q_and_A
+            // 
+            this.Q_and_A.Location = new System.Drawing.Point(4, 22);
+            this.Q_and_A.Name = "Q_and_A";
+            this.Q_and_A.Padding = new System.Windows.Forms.Padding(3);
+            this.Q_and_A.Size = new System.Drawing.Size(858, 474);
+            this.Q_and_A.TabIndex = 2;
+            this.Q_and_A.Text = "質問スレ";
+            this.Q_and_A.UseVisualStyleBackColor = true;
+            // 
+            // Question
+            // 
+            this.Question.Location = new System.Drawing.Point(4, 22);
+            this.Question.Name = "Question";
+            this.Question.Padding = new System.Windows.Forms.Padding(3);
+            this.Question.Size = new System.Drawing.Size(858, 474);
+            this.Question.TabIndex = 1;
+            this.Question.Text = "Gathers道場";
+            this.Question.UseVisualStyleBackColor = true;
+            // 
+            // SoarceCode
+            // 
+            this.SoarceCode.Location = new System.Drawing.Point(4, 22);
+            this.SoarceCode.Name = "SoarceCode";
+            this.SoarceCode.Padding = new System.Windows.Forms.Padding(3);
+            this.SoarceCode.Size = new System.Drawing.Size(858, 474);
+            this.SoarceCode.TabIndex = 0;
+            this.SoarceCode.Text = "ソースコード共有";
+            this.SoarceCode.UseVisualStyleBackColor = true;
+            // 
+            // MainPage
+            // 
+            this.MainPage.Controls.Add(this.tableLayoutPanel1);
+            this.MainPage.Location = new System.Drawing.Point(4, 22);
+            this.MainPage.Name = "MainPage";
+            this.MainPage.Padding = new System.Windows.Forms.Padding(3);
+            this.MainPage.Size = new System.Drawing.Size(858, 474);
+            this.MainPage.TabIndex = 5;
+            this.MainPage.Text = "メインページ";
+            this.MainPage.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.listBox1, 0, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30.46358F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.56954F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 53.25444F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(845, 177);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("メイリオ", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(839, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gathersへようこそ!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(3, 83);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(839, 88);
+            this.listBox1.TabIndex = 1;
+            // 
+            // MainTab
+            // 
+            this.MainTab.Controls.Add(this.MainPage);
+            this.MainTab.Controls.Add(this.SoarceCode);
+            this.MainTab.Controls.Add(this.Question);
+            this.MainTab.Controls.Add(this.Q_and_A);
+            this.MainTab.Controls.Add(this.reference);
+            this.MainTab.Location = new System.Drawing.Point(1, 25);
+            this.MainTab.Name = "MainTab";
+            this.MainTab.SelectedIndex = 0;
+            this.MainTab.Size = new System.Drawing.Size(866, 500);
+            this.MainTab.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(839, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "通知";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
             // MainFrame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1486, 596);
+            this.ClientSize = new System.Drawing.Size(867, 461);
             this.Controls.Add(this.toolbar);
             this.Controls.Add(this.MainTab);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -228,22 +293,19 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Gathers\'";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.MainTab.ResumeLayout(false);
             this.toolbar.ResumeLayout(false);
             this.toolbar.PerformLayout();
+            this.MainPage.ResumeLayout(false);
+            this.MainPage.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.MainTab.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TabControl MainTab;
-        private System.Windows.Forms.TabPage SoarceCode;
-        private System.Windows.Forms.TabPage Question;
-        private System.Windows.Forms.TabPage Q_and_A;
-        private System.Windows.Forms.TabPage reference;
-        private System.Windows.Forms.TabPage Gathers_Editor;
         private System.Windows.Forms.ToolStrip toolbar;
         private System.Windows.Forms.ToolStripButton 新規作成NToolStripButton;
         private System.Windows.Forms.ToolStripButton 開くOToolStripButton;
@@ -255,6 +317,16 @@
         private System.Windows.Forms.ToolStripButton 貼り付けPToolStripButton;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripButton ヘルプLToolStripButton;
+        private System.Windows.Forms.TabPage reference;
+        private System.Windows.Forms.TabPage Q_and_A;
+        private System.Windows.Forms.TabPage Question;
+        private System.Windows.Forms.TabPage SoarceCode;
+        private System.Windows.Forms.TabPage MainPage;
+        private System.Windows.Forms.TabControl MainTab;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
